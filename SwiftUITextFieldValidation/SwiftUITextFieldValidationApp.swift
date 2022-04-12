23c866f779cb16301bba055ff837265e994bc6cc
Jeff Spooner <jdspoone@icloud.com>
2022-04-12 10:14:28 -0600

    Initial Commit

---
+++ SwiftUITextFieldValidation/SwiftUITextFieldValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextFieldValidationApp.swift
+//  SwiftUITextFieldValidation
+//
+//  Created by Jeff Spooner on 2022-04-12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextFieldValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
